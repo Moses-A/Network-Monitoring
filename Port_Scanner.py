44bc3c666332625f10963edbef6b60bbb326a84c
Moses Arocha <carocha24@gmail.com>
2016-09-16 15:33:41 -0500

    Port Scanner That Grabs Application Banner

--- Port_Scanner.py
+++ Port_Scanner.py
@@ -8,39 +8,39 @@ from threading import *
 
 ThreadingDisplayLimit = Semaphore(value=1)		# Limits the display of the threading, so only one function is displayed at a time.
 
-def ConnectionScan(TargetHost, TargetPort):
+def ConnectionScan(Target_Host, Target_Port):
     try: # The attemption to send a SYN packet to create a TCP session.
         ConnectionToSocket = socket(AF_INET, SOCK_STREAM)	
-        ConnectionToSocket.connect((TargetHost, TargetPort))
+        ConnectionToSocket.connect((Target_Host, Target_Port))
         ConnectionToSocket.send('KaliWithUbuntu\r\n')		
         results = ConnectionToSocket.recv(100)
         ThreadingDisplayLimit.acquire()
         # If the package is successfully received, this tells the port scan it is open.
-        print '\n\t[Success] %d/ TCP Port Open'% TargetPort
+        print '\n\t[Success] %d/ TCP Port Open'% Target_Port
         print '\n\t[Success] ' + str(results)
     except:
         ThreadingDisplayLimit.acquire()
-        print '\t\t[Failure] %d/ TCP Port Closed'% TargetPort   # After the attemption TCP session creation, if it fails, the port is closed.
+        print '\t\t[Failure] %d/ TCP Port Closed'% Target_Port   # After the attemption TCP session creation, if it fails, the port is closed.
     finally:
     	# Required, most release the Display screen and close the socket if it was created.
         ThreadingDisplayLimit.release()
         ConnectionToSocket.close()		
 
-def PortScanner(TargetHost, TargetPorts):
+def PortScanner(Target_Host, Target_Ports):
     try:
-        TargetIP = gethostbyname(TargetHost)			 # Inclusion of a NS that will grab the IP's of URL's, if unknown.
+        Target_IP = gethostbyname(Target_Host)			 # Inclusion of a NS that will grab the IP's of URL's, if unknown.
     except:
-        print "\n\t[Failure] Cannot resolve '%s': Host Does Not Exist"%TargetHost
+        print "\n\t[Failure] Cannot resolve '%s': Host Does Not Exist"%Target_Host
         return
     try:
-        TargetName = gethostbyaddr(TargetIP)		 	  # Inclusion of reverse NS that will grab the URL's of the IP's 
-        print '\n\t[Success] Scan Results for: ' + TargetName[0]  
+        Target_Name = gethostbyaddr(Target_IP)		 	  # Inclusion of reverse NS that will grab the URL's of the IP's 
+        print '\n\t[Success] Scan Results for: ' + Target_Name[0]  
     except:							  
-        print '\n\t[Success] Scan Results for: ' + TargetIP
+        print '\n\t[Success] Scan Results for: ' + Target_IP
     setdefaulttimeout(1)					 # If no response is recevied from the port, it will time out to prevent overload.
 # The beginning of the threading for each port, it seperates the scans, and displays them individually, to speed up scan.
-    for TargetPort in TargetPorts:
-        threading = Thread(target=ConnectionScan, args=(TargetHost, int(TargetPort))) # References the ConnectionScan function
+    for Target_Port in Target_Ports:
+        threading = Thread(target=ConnectionScan, args=(Target_Host, int(Target_Port))) # References the ConnectionScan function
         threading.start()
 
 def main():
@@ -48,14 +48,14 @@ def main():
     extensions.add_option('-H', '--Host', dest = 'TargetHost', type='string', help = 'Please Write The Exact Host Targeted')
     extensions.add_option('-P', '--Port', dest = 'TargetPort', type='string', help = 'Please Write The Exact Ports, Only Seperated By Comma')
     (options,args) = extensions.parse_args()
-    TargetHost = options.TargetHost
-    TargetPorts = str(options.TargetPort).split(',')		# The seperation of the ports, that inserts into the list
-    if (TargetHost == None) | (TargetPorts[0] == None):  	# The creation of a quick list of all of the Ports, it grabs the first.
+    Target_Host = options.TargetHost
+    Target_Ports = str(options.TargetPort).split(',')		# The seperation of the ports, that inserts into the list
+    if (Target_Host == None) | (Target_Ports[0] == None):  	# The creation of a quick list of all of the Ports, it grabs the first.
         print extensions.usage
         exit(0)
     if not os.geteuid() == 0:
         sys.exit('Must Be Root!')
-    PortScanner(TargetHost, TargetPorts)			# The reference to PortScanner function must be last to go through all error handling.
+    PortScanner(Target_Host, Target_Ports)			# The reference to PortScanner function must be last to go through all error handling.
 
 if __name__ == "__main__":
     main()
